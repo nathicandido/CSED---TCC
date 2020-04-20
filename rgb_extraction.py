6d86bbbdd1827577211fbe46dc18bd4e68dd99d6
nathicandido <36238224+nathicandido@users.noreply.github.com>
2020-04-19 21:49:27 -0300

    Update rgb_extraction.py

--- rgb_extraction.py
+++ rgb_extraction.py
@@ -5,4 +5,4 @@ def lk(image):
     myimg = cv2.imread(image)
     avg_color_per_row = numpy.average(myimg, axis=0)
     avg_color = numpy.average(avg_color_per_row, axis=0)
-    print(avg_color)
+    return avg_color
