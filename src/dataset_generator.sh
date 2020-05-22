94ebfdc6069ce8cf748ac64555622c35f74c5645
Rafael Silva <rafaelsm_1993@hotmail.com>
2020-05-21 21:52:27 -0300

    dataset_generator

---
+++ src/dataset_generator.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#VARIABLES
+ASSETS_FOLDER="../assets"
+MANEUVER=$1
+MAIN=$(find . -name main.py)
+
+#for all maneuver in dataset folder
+for i in $(ls -d $ASSETS_FOLDER/$MANEUVER*);
+do
+	echo "----------EXEC - $i----------"
+	VAR="$MAIN --parse --maneuver $MANEUVER --video_path_list /$i/front.mp4;/$i/left.mp4;/$i/rear.mp4;/$i/right.mp4 --dump_buffer"
+	python $VAR
+done
