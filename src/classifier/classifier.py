9d63537978013601381d53c0121eac52330f4a56
Rafael Silva <rafaelsm_1993@hotmail.com>
2020-05-25 01:02:34 -0300

    classifier - not finished
    Description:
    - implementing classifier, is not finished

--- src/classifier/classifier.py
+++ src/classifier/classifier.py
@@ -1,11 +1,14 @@
+import os
+
 from keras import backend
 from keras.layers import Dense, Activation, Flatten, Dropout, LeakyReLU
 from keras.models import Sequential
-from keras.optimizers import SGD
 from keras.utils.generic_utils import get_custom_objects
 from tensorflow import ConfigProto, Session, function
+from sklearn.model_selection import StratifiedKFold
 import keras
 import numpy as np
+import pickle as pkl
 
 
 class Classifier:
@@ -30,23 +33,70 @@ class Classifier:
     @classmethod
     def run(cls):
 
-        # full_array = np.array([np.array([x, y]) for x, y in zip(arr_x, arr_y)])
-        # result = np.array([[1, 0, 0, 0], [0, 0, 0, 1], [0, 1, 0, 0], [1, 0, 0, 0], [0, 0, 1, 0]])
-        # full_test = np.array([np.array([x, y]) for x, y in zip(test_x, test_y)])
+        classifier = cls.create_classifier()
+        original_train_dataset, original_train_labels = cls.get_dataset()
 
+        # Will return a list of sliced dataset, to use cross validation
+        train_dataset_list, train_labels_list, test_dataset_list, test_labels_list = \
+            cls.split_dataset_with_cross_validation(original_train_dataset, original_train_labels)
+
+        for train_dataset, train_labels, test_dataset, test_labels in zip(train_dataset_list, train_labels_list, test_dataset_list, test_labels_list):
+            classifier.fit(train_dataset, train_labels, batch_size=1, epochs=100)
+
+            # To validate prediction_list with test_labels.
+            # So, will be possible to confirm the accuracy for this sliced list
+            prediction_list = classifier.predict(test_dataset)
+
+        # will be necessary to confirm all predicted values, and will be possible to determine the accuracy for our model/classifier.
+
+
+    @classmethod
+    def create_classifier(self):
         # Creating 4 Layers (2*100, 2*52, 2*26, 4)
         classifier = Sequential()
         classifier.add(LeakyReLU(alpha=0.3, input_shape=(2, 100)))
         classifier.add(Dense(units=52, activation="relu"))
         classifier.add(Dense(units=26, activation="relu"))
         classifier.add(Flatten())
-
         classifier.add(Dense(units=4, activation="softmax"))
         classifier.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
 
-        # classifier.fit(TRAINNING, RESULTS, batch_size=1, epochs=10)
+        return classifier
 
-        # classifier.predict(TEST))
+    @classmethod
+    def get_dataset(cls):
+        with open(os.path.join(os.getcwd(), '..', 'dataset', 'ready_for_training.pkl'), 'rb') as f:
+            data = pkl.load(f)
+            labels = {"SWERVING": [1, 0, 0, 0],
+                      "ULTRAPASSA": [0, 1, 0, 0],
+                      "FALSAULTRAPASSA": [0, 0, 1, 0],
+                      "MUDAFAIXA": [0, 0, 0, 1]}
+            train_dataset = np.array([np.array([index['x_sig'], index['y_sig']]) for index in data])
+            train_label = np.array([(labels[index['label']]) for index in data])
+            return train_dataset, train_label
+
+    @classmethod
+    def split_dataset_with_cross_validation(cls, dataset, labels, k=5):
+        splitter = StratifiedKFold(n_splits=k)
+        cross_validation = splitter.split(dataset, labels)
+        crossed_train_dataset_list = list()
+        crossed_train_labels_list = list()
+        crossed_test_dataset_list = list()
+        crossed_test_labels_list = list()
+        for train_index, test_index in cross_validation:
+            train_dataset, train_labels, test_dataset, test_labels = cls.split_dataset(train_index, test_index, dataset, labels)
+            crossed_train_dataset_list.append(train_dataset)
+            crossed_train_labels_list.append(train_labels)
+            crossed_test_dataset_list.append(test_dataset)
+            crossed_test_labels_list.append(test_labels)
+        return crossed_train_dataset_list, crossed_train_labels_list, crossed_test_dataset_list, crossed_test_labels_list
+
+    @classmethod
+    def split_dataset(cls, train_index, test_index, dataset, labels):
+        train_dataset, train_labels = [(dataset[index], labels[index]) for index in train_index]
+        test_dataset, test_labels = [(dataset[index], labels[index]) for index in test_index]
+        return train_dataset, train_labels, test_dataset, test_labels
+
 
 if __name__ == '__main__':
     Classifier()
