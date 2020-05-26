cf8a90640b68baca59b30ef964b88495507de151
Tiago Arrazi <tiago_arrazi98@outlook.com>
2020-05-25 23:55:26 -0300

    final version of classifier

--- src/classifier/classifier.py
+++ src/classifier/classifier.py
@@ -1,39 +1,33 @@
 import os
 
 from keras import backend
-from keras.layers import Dense, Activation, Flatten, Dropout, LeakyReLU
+from keras.layers import Dense, Flatten, LeakyReLU
 from keras.models import Sequential
-from keras.utils.generic_utils import get_custom_objects
-from tensorflow import ConfigProto, Session, function
+from tensorflow import ConfigProto, Session
 from sklearn.model_selection import StratifiedKFold
-import keras
 import numpy as np
 import pickle as pkl
+from time import time
 
 
 class Classifier:
 
-    def __init__(self):
-        @function
-        def leaky_relu(x, alpha=0.01):
-            print(f'(Func = {x.op}')
-            if x.op < 0:
-                x.op = x.op * alpha
-                return x
-            return x
+    LABELS = {"['SWERVING']": np.array([1, 0, 0, 0]),
+              "['ULTRAPASSA']": np.array([0, 1, 0, 0]),
+              "['FALSAULTRAPASSA']": np.array([0, 0, 1, 0]),
+              "['MUDAFAIXA']": np.array([0, 0, 0, 1])}
 
-        get_custom_objects().update(dict(leaky_relu=Activation(leaky_relu)))
+    def __init__(self):
 
         config = ConfigProto(device_count=dict(GPU=1, CPU=2))
         sess = Session(config=config)
-        self.run()
         backend.set_session(sess)
+        self.run()
         backend.clear_session()
 
     @classmethod
     def run(cls):
 
-        classifier = cls.create_classifier()
         original_train_dataset, original_train_labels = cls.get_dataset()
 
         # Will return a list of sliced dataset, to use cross validation
@@ -41,17 +35,40 @@ class Classifier:
             cls.split_dataset_with_cross_validation(original_train_dataset, original_train_labels)
 
         for train_dataset, train_labels, test_dataset, test_labels in zip(train_dataset_list, train_labels_list, test_dataset_list, test_labels_list):
-            classifier.fit(train_dataset, train_labels, batch_size=1, epochs=100)
+            train_dataset = np.array(train_dataset)
+            train_labels = np.array(list(map(lambda each: cls.LABELS[each], train_labels)))
+
+            classifier = cls.create_classifier()
+
+            classifier.fit(train_dataset, train_labels, batch_size=10, epochs=100)
 
             # To validate prediction_list with test_labels.
             # So, will be possible to confirm the accuracy for this sliced list
+
+            test_dataset = np.array(test_dataset)
+            test_labels = np.array(list(map(lambda each: cls.LABELS[each], test_labels)))
+
             prediction_list = classifier.predict(test_dataset)
 
-        # will be necessary to confirm all predicted values, and will be possible to determine the accuracy for our model/classifier.
+            t = str(time())
+            np.savez(
+                f'C:/Users/Tiagoo/PycharmProjects/CSED---TCC/dataset/results/result_{t.replace(".", "_")}.npz',
+                prediction=prediction_list,
+                expected=test_labels
+            )
 
+            acc = 0
+            for pred, expected in zip(prediction_list, test_labels):
+                if np.argmax(pred) == np.argmax(expected):
+                    acc += 1
+
+            print(acc / len(prediction_list))
+
+        # will be necessary to confirm all predicted values,
+        # and will be possible to determine the accuracy for our model/classifier.
 
     @classmethod
-    def create_classifier(self):
+    def create_classifier(cls):
         # Creating 4 Layers (2*100, 2*52, 2*26, 4)
         classifier = Sequential()
         classifier.add(LeakyReLU(alpha=0.3, input_shape=(2, 100)))
@@ -67,16 +84,12 @@ class Classifier:
     def get_dataset(cls):
         with open(os.path.join(os.getcwd(), '..', 'dataset', 'ready_for_training.pkl'), 'rb') as f:
             data = pkl.load(f)
-            labels = {"SWERVING": np.array([1, 0, 0, 0]),
-                      "ULTRAPASSA": [0, 1, 0, 0],
-                      "FALSAULTRAPASSA": [0, 0, 1, 0],
-                      "MUDAFAIXA": [0, 0, 0, 1]}
-            train_dataset = np.array([np.array([index['x_sig'], index['y_sig']]) for index in data])
-            train_label = np.array([(labels[index['label']]) for index in data])
+            train_dataset = np.array([[index['x_sig'], index['y_sig']] for index in data])
+            train_label = np.array([np.array2string(index['label']) for index in data])
             return train_dataset, train_label
 
     @classmethod
-    def split_dataset_with_cross_validation(cls, dataset, labels, k=5):
+    def split_dataset_with_cross_validation(cls, dataset, labels, k=3):
         splitter = StratifiedKFold(n_splits=k)
         cross_validation = splitter.split(dataset, labels)
         crossed_train_dataset_list = list()
@@ -93,8 +106,16 @@ class Classifier:
 
     @classmethod
     def split_dataset(cls, train_index, test_index, dataset, labels):
-        train_dataset, train_labels = [(np.array(dataset[index]), np.array(labels[index])) for index in train_index]
-        test_dataset, test_labels = [(np.array(dataset[index]), np.array(labels[index])) for index in test_index]
+        train_dataset, train_labels = [
+            [dataset[index] for index in train_index],
+            [labels[index] for index in train_index]
+        ]
+
+        test_dataset, test_labels = [
+            [dataset[index] for index in test_index],
+            [labels[index] for index in test_index]
+        ]
+
         return train_dataset, train_labels, test_dataset, test_labels
 
 
